7b26eeb92512f329accef7d2c0c7311f72f50590
oab1 <oab1@users.sourceforge.net>
2023-03-07 23:06:37 +0000

    Part of assets refiling

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import CatRoutes from "./client/components/Routes";
-import "./client/App.css";
+import "./client/asset/App.css";
  
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
