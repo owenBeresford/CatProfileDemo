00a2ac2b1f1666a94833945a4b5a672bed94284b
oab1 <oab1@users.sourceforge.net>
2024-01-05 20:24:55 +0000

    Extend 'make' script

--- src/build-tools/post-build.mjs
+++ src/build-tools/post-build.mjs
@@ -1,4 +1,4 @@
-import { readFile, writeFile, mkdir, copyFile } from "fs/promises";
+import { readFile, writeFile, mkdir, copyFile, unlink } from "fs/promises";
 import { constants, lstatSync, createWriteStream } from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -6,12 +6,14 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const BUILD_DIR = path.join(__dirname, "..", "..", "build");
+const EXCESS_CSS = path.join(__dirname, "..", "..", "build", "App.css");
 const IN_FILE = path.join(BUILD_DIR, "index.html");
 const FIXTURE_OUT_DIR = path.join(__dirname, "..", "..", "build", "fixtures");
 const FIXTURE_IN_DIR = path.join(__dirname, "..", "server", "fixtures");
 const ASSET_IN_DIR = path.join(__dirname, "..", "client", "asset");
+const PUBLIC_DIR = path.join(__dirname, "..", "..", "public");
 
-const isDir = lstatSync(FIXTURE_OUT_DIR, { throwIfNoEntry: false });
+let isDir = lstatSync(FIXTURE_OUT_DIR, { throwIfNoEntry: false });
 if (!isDir) {
   await mkdir(FIXTURE_OUT_DIR).catch((ee) => {
     console.error("File mkdir error ", ee);
@@ -38,5 +40,19 @@ await copyFile(
 ).catch((ee) => {
   console.error("File copy error ", ee);
 }); // add abort?
-
 console.log("Copied fixtures");
+
+isDir = lstatSync(PUBLIC_DIR, { throwIfNoEntry: false });
+if (isDir) {
+  await unlink(PUBLIC_DIR).catch((ee) => {
+    console.error("File unlink error ", ee);
+  }); // add abort?
+}
+isDir = lstatSync(EXCESS_CSS , { throwIfNoEntry: false });
+if (isDir) {
+  await unlink(EXCESS_CSS ).catch((ee) => {
+    console.error("File unlink error ", ee);
+  }); // add abort?
+}
+console.log("Unlinked excess build copies");
+
